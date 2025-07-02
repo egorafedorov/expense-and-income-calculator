702474065ca8ba3abd5382facf5c79f7f1fff48f
egorafedorov <egorafedorovme@gmail.com>
2025-07-02 10:44:49 +0300

    iteration 01

--- main.go
+++ main.go
@@ -3,5 +3,20 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Print("start")
+	var transactions []float64
+	for {
+		transaction := scanTransaction()
+		if transaction == 0.0 {
+			break
+		}
+		transactions = append(transactions, transaction)
+	}
+	fmt.Printf("Result: %.2f", transactions)
+}
+
+func scanTransaction() float64 {
+	var transaction float64
+	fmt.Print("Enter transaction (N to exit): ")
+	fmt.Scan(&transaction)
+	return transaction
 }
